de758a96d3b12d47a9226fab147ca269761c3414
abdullah0007 <sabdullah.978@gmail.com>
2017-07-25 13:14:24 +0500

    front end changes

--- views/home.php
+++ views/home.php
@@ -3,6 +3,36 @@
 require('layout/header.php'); ?>
 
 <!-- Content Goes Here -->
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="bus-board">
+                    <h1 class="title-board"><strong>BUS NAME</strong></h1>
+                    <div class="plank">
+                        <p class="seats-avaliable">20 Seats are remaining</p>
+                        <button type="button" class="btn btn-primary btn-flat">Get a Seat</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="bus-board">
+                    <div class="plank">
+                        <p class="bus-name">Some Bus Name</p>
+                        <p class="seats-avaliable">20 Seats are remaining</p>
+                        <button type="button" class="btn btn-primary btn-flat">Get a Seat</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="bus-board">
+                    <div class="plank">
+                        <p class="bus-name">Some Bus Name</p>
+                        <p class="seats-avaliable">20 Seats are remaining</p>
+                        <button type="button" class="btn btn-primary btn-flat">Get a Seat</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <!-- Content Ends Here -->
 <?php require('layout/footer.php'); ?>
