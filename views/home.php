bd6ea960259da511888c518ac799b9a22a088964
abdullah0007 <sabdullah.978@gmail.com>
2017-07-25 13:49:41 +0500

    home page design chagnes

--- views/home.php
+++ views/home.php
@@ -9,30 +9,34 @@ require('layout/header.php'); ?>
                 <div class="bus-board">
                     <h1 class="title-board"><strong>BUS NAME</strong></h1>
                     <div class="plank">
+                        <p class="departure-time">3 Hours from Now</p>
                         <p class="seats-avaliable">20 Seats are remaining</p>
-                        <button type="button" class="btn btn-primary btn-flat">Get a Seat</button>
+                        <button type="button" class="btn btn-custom-primary btn-flat">Get a Seat</button>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="bus-board">
+                    <h1 class="title-board"><strong>BUS NAME</strong></h1>
                     <div class="plank">
-                        <p class="bus-name">Some Bus Name</p>
+                        <p class="departure-time">3 Hours from Now</p>
                         <p class="seats-avaliable">20 Seats are remaining</p>
-                        <button type="button" class="btn btn-primary btn-flat">Get a Seat</button>
+                        <button type="button" class="btn btn-custom-primary btn-flat">Get a Seat</button>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="bus-board">
+                    <h1 class="title-board"><strong>BUS NAME</strong></h1>
                     <div class="plank">
-                        <p class="bus-name">Some Bus Name</p>
+                        <p class="departure-time">3 Hours from Now</p>
                         <p class="seats-avaliable">20 Seats are remaining</p>
-                        <button type="button" class="btn btn-primary btn-flat">Get a Seat</button>
+                        <button type="button" class="btn btn-custom-primary btn-flat">Get a Seat</button>
                     </div>
                 </div>
             </div>
         </div>
+        
     </div>
 <!-- Content Ends Here -->
 <?php require('layout/footer.php'); ?>
--- views/layout/header.php
+++ views/layout/header.php
@@ -7,7 +7,31 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,700i,800|Raleway:100,200,300,300i,400,400i,500,500i,600,700,700i,800,900" rel="stylesheet">
         <link href="assets/app.css" rel="stylesheet">
     </head>
     <body>
-    
+    <header class="header">
+        <div class="container">
+            <div class="menu menut-top">
+            <form class="form-horizontal">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="destination">Where you want to go?</label>
+                        <select name="destination" class="form-control form-custom-control">
+                            <option value="islamabad">Islamabad</option>
+                            <option value="lahore">Lahore</option>
+                            <option value="Rawalpindi">Rawalpindi</option>
+                            <option value="Gujrat">Gujrat</option>
+                            <option value="dikhan">DI Khan</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-6" style="padding-top:23px;">
+                    <button type="submit" class="btn btn-custom-primary btn-flat">Get a Bus</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </header>
+    <div class="clearfix"></div>
